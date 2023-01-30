91330ddcf4e0bc4323631eba1c2f89dc883d8101
Alexandra W <lexa.ward96@icloud.com>
2023-01-30 11:11:32 +0000

    Filtering lists

--- Conditional Logic/loops/for.scala
+++ Conditional Logic/loops/for.scala
@@ -1,10 +1,17 @@
 object forLoop{
     def main(args: Array[String]){
+        val lst = List(1,2,3,4,5,6,7,8,9,10)
         for(i <- 1.to(5)){
             println(i)
         }
         for(i <- 1 until 6){
             println(f"i using until $i%d")
+        }
+        for(i <- lst){
+            println(i)
+        }
+        for(i <- lst; if i % 2 == 0){
+            println(i)
         }
     }
 }
